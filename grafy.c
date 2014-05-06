c9225cf3a5c84720534a897432481b44409857a4
Wojciech Lisiecki <wojciech.j.lisiecki@gmail.com>
2014-05-06 11:23:18 +0200

    BFS i DFS w produkcji

--- grafy.c
+++ grafy.c
@@ -3,6 +3,8 @@
 #include <time.h>
 
 int i,j;
+int n;
+int *visited;
 
 struct list
 {
@@ -19,6 +21,13 @@ struct list2
 };
 typedef struct list2 sedgeList;
 
+struct BFSl
+{
+    struct BFSl *next;
+    int value;
+};
+typedef struct BFSl BFS_list;
+
 void adjmatrix_gen(int n, int *adjmatrix[])     //generowanie macierzy sasiedztwa
 {
     for (i=0; i<n; i++)
@@ -31,6 +40,32 @@ void adjmatrix_gen(int n, int *adjmatrix[])     //generowanie macierzy sasiedztw
     }
 }
 
+void adjmatrix_read(int *adjmatrix[])
+{
+    for (i=0; i<n; i++)
+    {
+        for (j=0; j<n; j++)
+        {
+            adjmatrix[i][j] = 0;
+        }
+    }
+    int x,y;
+    /*
+    for (i=0; i<=n; i++)
+    {
+        scanf("%d",&x);
+        scanf("%d",&y);
+        adjmatrix[x][y] = 1;
+    }*/
+    adjmatrix[0][1] = 1;
+    adjmatrix[0][2] = 1;
+    adjmatrix[1][3] = 1;
+    adjmatrix[1][4] = 1;
+    adjmatrix[2][3] = 1;
+    adjmatrix[2][4] = 1;
+    adjmatrix[3][4] = 1;
+}
+
 void adjmatrix_print(int n, int *adjmatrix[])      //wyswietlanie macierzy sasiedztwa
 {
     printf("\nMACIERZ SASIEDZTWA: \n");
@@ -41,6 +76,53 @@ void adjmatrix_print(int n, int *adjmatrix[])      //wyswietlanie macierzy sasie
             printf("%d\t",adjmatrix[i][j]);
         }
         printf("\n");
+    }
+}
+
+void DFS_adjmatrix(int* adjmatrix[], int w)
+{
+    visited[w] = 1;
+    printf("%d",w);
+    for (i=0; i<n; i++)
+    {
+        if ((adjmatrix[w][i] == 1) && (visited[i] == 0)) DFS_adjmatrix(adjmatrix, i);
+    }
+}
+
+void BFS_adjmatrix(int *adjmatrix[], int w)
+{
+    BFS_list *newEl, *head, *tail;
+
+    newEl = (BFS_list*)malloc(sizeof(BFS_list));
+    newEl->next = NULL;
+    newEl->value = w;
+    head = tail = newEl;
+
+    visited[w] = 1;
+
+    while(head)
+    {
+        w = head->value;
+        newEl = head;
+        head = head->next;
+        if(!head) tail = NULL;
+        free(newEl);
+
+        printf("%d",w);
+
+        for (i=0; i<n; i++)
+        {
+            if ((adjmatrix[w][i] == 1) && (visited[i] == 0))
+            {
+                newEl = (BFS_list*)malloc(sizeof(BFS_list));
+                newEl->next = NULL;
+                newEl->value = i;
+                if(!tail) head = newEl;
+                else tail->next = newEl;
+                tail = newEl;
+                visited[i] = 1;
+            }
+        }
     }
 }
 
@@ -84,6 +166,47 @@ void adjList_print(int n, int* adjlist)         //wyswietlanie listy sasiedztwa
     }
 }
 
+void BFS_adjlist(int *adjlist, int w)
+{
+    BFS_list *newEl, *tmp, *head, *tail;
+
+    newEl = (BFS_list*)malloc(sizeof(BFS_list));
+    newEl->next = NULL;
+    newEl->value = w;
+    head = tail = newEl;
+
+    visited[w] = 1;
+
+    while(head)
+    {
+        w = head->value;
+        newEl = head;
+        head = head->next;
+        if(!head) tail = NULL;
+        free(newEl);
+
+        printf("%d",w);
+
+        //for (tmp = adjlist[w]; tmp; tmp = tmp->next)
+        tmp = adjlist[w];
+        while(tmp)
+        {
+            if (visited[tmp->value] == 0)
+            {
+                newEl = (BFS_list*)malloc(sizeof(BFS_list));
+                newEl->next = NULL;
+                newEl->value = tmp->value;
+                if(!tail) head = newEl;
+                else tail->next = newEl;
+                tail = newEl;
+                visited[tmp->value] = 1;
+            }
+            tmp = tmp->next;
+        }
+    }
+}
+
+
 void edgeList_gen(int n, sedgeList **head, int **adjmatrix)         //tworzenie listy krawedzi
 {
     sedgeList *newEE;
@@ -118,9 +241,9 @@ void edgeList_print(sedgeList *head)        //wyswietlanie listy krawedzi
 int main()
 {
     srand(time(NULL));
-    int n;
     printf("Podaj liczbe wierzcholkow: \n");
     scanf("%d",&n);
+    visited = (int*)malloc(n*sizeof(int));
 
     //INICJALIZACJA TABLICY n x n - macierz sasiedztwa
     int **adjmatrix;
@@ -131,8 +254,19 @@ int main()
     }
 
     //MACIERZ SASIEDZTWA
+    //adjmatrix_read(adjmatrix);
     adjmatrix_gen(n, adjmatrix);
     adjmatrix_print(n, adjmatrix);
+
+    for (i=0; i<n; i++) visited[i] = 0;
+    printf("----------\n");
+    DFS_adjmatrix(adjmatrix, 0);
+    printf("\n----------");
+
+    for (i=0; i<n; i++) visited[i] = 0;
+    printf("----------\n");
+    BFS_adjmatrix(adjmatrix, 0);
+    printf("\n----------");
 
     //INICJALIZACJA - lista sasiedztwa
     slistEl ** adjlist;
@@ -142,6 +276,11 @@ int main()
     //LISTA SASIEDZTWA
     adjList_gen(n, adjlist, adjmatrix);
     adjList_print(n, adjlist);
+
+    for (i=0; i<n; i++) visited[i] = 0;
+    printf("----------\n");
+    BFS_adjlist(adjlist, 0);
+    printf("\n----------");
 
     //INICJALIZACJA -lista krawedzi
     sedgeList *head, *newEE, *currEE;
